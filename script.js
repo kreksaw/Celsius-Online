ada41855b5acde4f45f1a1719b42a820ebd3ca26
kreksaw <kreksaw@gmail.com>
2026-05-23 11:42:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -458,7 +458,7 @@ const revealObserver = new IntersectionObserver(
       }
     });
   },
-  { threshold: 0.12, rootMargin: "0px 0px -40px 0px" }
+  { threshold: 0, rootMargin: "0px 0px -10% 0px" }
 );
 
 document.querySelectorAll(".reveal").forEach((el) => revealObserver.observe(el));
